071a01f35b9f351c7c2e0fbf019112e0eb4e8af2
innesbre <brendan.innes@mail.utoronto.ca>
2020-01-10 12:54:06 -0500

    Fixed bug in pie charts where Id column of cluster.metadata was included in pie chart

--- R/Visualize.R
+++ R/Visualize.R
@@ -31,16 +31,17 @@ PlotTrajectory <- function(object){
 
   if (length(levels(object@meta.data$Timepoints)) > 9){
     colours <- colorRampPalette(RColorBrewer::brewer.pal(7, "YlOrRd"))
-    plot.igraph(edge_graph, ylim=c(-1,1), layout = l$layout, ylab = "Inferred time", vertex.shape = "pie", vertex.pie = lapply(1:nrow(object@cluster.metadata), function(x) as.numeric(object@cluster.metadata[x,1:(length(levels(object@meta.data$Timepoints)))])),
+    plot.igraph(edge_graph, ylim=c(-1,1), layout = l$layout, ylab = "Inferred time", vertex.shape = "pie", vertex.pie = lapply(1:nrow(object@cluster.metadata), function(x) as.numeric(object@cluster.metadata[x,levels(object@meta.data$Timepoints)])),
          vertex.pie.color=list(colours(length(levels(object@meta.data$Timepoints)))), pie.border=list(rep("white", 4)), vertex.frame.color="white", edge.arrow.size = 0.8, edge.width = 1.5, vertex.label.family="Arial",
          vertex.label.color="black", edge.lty = E(edge_graph)$type, vertex.label.cex=1.5)
     axis(side=2, at=c(-1,1), labels=c("Late","Early"), las=1)
     legend("topleft", legend = levels(object@meta.data$Timepoints), fill=colours, bty = "n", border="black")
   } else {
     colours <- brewer.pal(length(levels(object@meta.data$Timepoints)), "YlOrRd")
-    plot.igraph(edge_graph, ylim=c(-1,1), ylab = "Inferred time", layout = l$layout, vertex.shape = "pie", vertex.pie = lapply(1:nrow(object@cluster.metadata), function(x) as.numeric(object@cluster.metadata[x,1:(length(levels(object@meta.data$Timepoints)))])),
-         vertex.pie.color=list(colours), pie.border=list(rep("white", length(levels(object@meta.data$Timepoints)))), vertex.frame.color="white",
-         edge.arrow.size = 0.8, edge.width = 1.5, vertex.label.family="Arial", vertex.label.color="black", edge.lty = E(edge_graph)$type, vertex.label.cex=1.5, vertex.size = 21)
+    plot.igraph(edge_graph, ylim=c(-1,1), ylab = "Inferred time", layout = l$layout, vertex.shape = "pie",
+                vertex.pie = lapply(1:nrow(object@cluster.metadata), function(x) as.numeric(object@cluster.metadata[x,levels(object@meta.data$Timepoints)])),
+                vertex.pie.color=list(colours), pie.border=list(rep("white", length(levels(object@meta.data$Timepoints)))), vertex.frame.color="white",
+                edge.arrow.size = 0.8, edge.width = 1.5, vertex.label.family="Arial", vertex.label.color="black", edge.lty = E(edge_graph)$type, vertex.label.cex=1.5, vertex.size = 21)
     legend("topleft", legend = levels(object@meta.data$Timepoints), fill=colours, bty = "n", border = "black")
     axis(side=2, at=c(-1,1), labels=c("Late","Early"), las=1)
   }
